d845c6976ad87909d610450fe1a97e5621798a45
san-holy <1328856429@qq.com>
2026-01-15 13:09:04 +0800

    everyone has a mutex but stuck

--- core.cpp
+++ core.cpp
@@ -11,6 +11,7 @@ private:
 public:
     int id;
     double balance;
+    std::mutex accountmtx;//加锁之后，不可以使用vector<Account>,因为锁不可以被移动和复制
     Account(int id_,int balance_)
         :id(id_),balance(balance_) {}
 };
@@ -18,22 +19,29 @@ public:
 class Bank{
     
 public:
-    std::mutex bankmtx;
 
-    std::vector<Account> accounts;
+    std::vector<std::unique_ptr<Account>> accounts;//所以我们用指针
     Bank(int n,double initial_balance){
         for(int i=0;i<n;i++){
-            accounts.push_back(Account(i,initial_balance));
+            //auto tmpptr=std::make_unique<Account>(i,initial_balance);
+            //accounts.push_back(std::move(tmpptr));可以这样写
+
+            accounts.push_back(std::move(std::make_unique<Account>(i,initial_balance)));
+            //也可以这么写，直接生成一个指针并移动并添加
         }
     }
 
     void transfer(int from,int to,double amount){
 
         {
-            bankmtx.lock();
-            accounts[from].balance-=amount;
-            accounts[to].balance+=amount;
-            bankmtx.unlock();
+            accounts[from]->accountmtx.lock();
+                accounts[to]->accountmtx.lock();
+                
+                accounts[from]->balance-=amount;
+                accounts[to]->balance+=amount;
+            
+                accounts[to]->accountmtx.unlock();
+            accounts[from]->accountmtx.lock();
         }
 
     }
@@ -41,11 +49,11 @@ public:
     double getTotalBalance(){
         double sum=0;
     {
-        bankmtx.lock();
         for(int i=0;i<accounts.size();i++){
-            sum+=accounts[i].balance;
+            //accounts[i]->accountmtx.lock();
+            sum+=accounts[i]->balance;
+            //accounts[i]->accountmtx.unlock();
         }
-        bankmtx.unlock();
     }
         return sum;
     }
